c28de5346475b85ab4e0969798880243b1d60650
Brian Mahabir <bmahabir@bu.edu>
2020-02-26 19:44:00 -0500

    3c complete

--- testproblem3/Problem3b.cpp
+++ testproblem3/Problem3b.cpp
@@ -4,17 +4,49 @@
 #include <iostream>
 using namespace std;
 
+
+void insert(int new_data, ListNode *head) {
+	ListNode* new_node = NULL;
+	new_node->val = new_data;
+	new_node->next = head;
+	head = new_node;
+}
+
+void display(ListNode *head) {
+	ListNode* ptr;
+	ptr = head;	
+	while (ptr != NULL) {
+		cout << ptr->val << " ";
+		ptr = ptr->next;
+	}
+}
+
+
 ListNode* insertionSortList(ListNode* head) {
 
-	ListNode* sorted = NULL;
-	ListNode *current = head;
+	 ListNode* sorted = NULL; //create struct object called sorted
+	 ListNode* current = head; //create struct pointer called current and point it to input head
 	
 	while (current != NULL)
 	{
-		ListNode* next = current->next;
-
+		
+		ListNode* nextnode = current->next; //create a list that always points to the next node of current
+		
+		
 		
 	}
 
-	*head = sorted
+	head = sorted;
+}
+
+int main(void)
+{
+	insert(3);
+	insert(1);
+	insert(7);
+	insert(2);
+	insert(9);
+	cout << "The linked list is: ";
+	display();
+	return 0;
 }
--- testproblem3/Problem3c.cpp
+++ testproblem3/Problem3c.cpp
@@ -4,18 +4,54 @@
 #include <iostream>
 using namespace std;
 
-ListNode* findCycleStart(ListNode* head) {
 
-	ListNode *current = head;
 
-	while (current != NULL)
+
+
+int IsCycle_maxsize(ListNode *head) // use two pointer method to see if there is a 
+//cycle and how much iterations it took to find the cycle to get a relative size
+{
+	ListNode* slow = head;
+	ListNode* fast = head;
+
+	for (int i = 0; slow->next && fast->next; i++)
 	{
-		ListNode* next = current->next;
-		ListNode* next2 = current->next->next;
-		if(next == next2)
+		slow = slow->next;
+		fast = fast->next->next;
+		if (slow->val == fast->val)
 		{
-			return 
+			return i;
 		}
 	}
-	return;
+}
+
+bool NodeinCycle(ListNode *head) // uses the max size found in the first function
+//to check whether a given node is part of the cycle.
+{
+	int key = head->val;
+	int size = IsCycle_maxsize(head);
+	for (int i = 0; head->next && i < size*2; i++)
+	{
+		if (head->val == key)
+		{
+			return true;
+		}
+	}
+	return false;
+}
+
+ListNode* findCycleStart(ListNode* head) //uses function NodeinCycle, 
+//the first node that is in the cycle is the start of the cycle
+{ 
+
+	ListNode *current = head;
+	while (current->next)
+	{
+		current = current->next;
+		if (NodeinCycle(current))
+		{
+			return current;
+		}
+	}
+	return NULL;
 }
